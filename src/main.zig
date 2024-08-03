303483160c2a98f7564097f34dceb9b4c10dfd84
Nhân <hi@imnhan.com>
2024-08-03 18:10:54 +0700

    use sqlite3_errmsg

--- src/main.zig
+++ src/main.zig
@@ -3,17 +3,18 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() !void {
-    var optionalDb: ?*c.sqlite3 = undefined;
-    if (c.sqlite3_open("db.sqlite3", &optionalDb) != c.SQLITE_OK) {
+    var optional_db: ?*c.sqlite3 = undefined;
+    const init_err = c.sqlite3_open("db.sqlite3", &optional_db);
+    defer _ = c.sqlite3_close(optional_db);
+    if (init_err != c.SQLITE_OK) {
+        print("ERR: {s}\n", .{c.sqlite3_errmsg(optional_db)});
         return error.SqliteOpenFail;
     }
-    defer _ = c.sqlite3_close(optionalDb);
-    const db: *c.sqlite3 = optionalDb orelse unreachable;
-    print(">> {d}\n", .{db});
+    const db: *c.sqlite3 = optional_db orelse unreachable;
 
     var stmt: ?*c.sqlite3_stmt = undefined;
-    _ = c.sqlite3_prepare_v2(db, "select 'hello';", -1, &stmt, null);
+    _ = c.sqlite3_prepare_v2(db, "select sqlite_version();", -1, &stmt, null);
     while (c.sqlite3_step(stmt) != c.SQLITE_DONE) {
-        print(">> {s}\n", .{c.sqlite3_column_text(stmt, 0)});
+        print("sqlite version: {s}\n", .{c.sqlite3_column_text(stmt, 0)});
     }
 }
