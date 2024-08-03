087092f867218fdc3988f08cf33c353712f8261b
Nhân <hi@imnhan.com>
2024-08-03 17:41:48 +0700

    hello world

--- src/main.zig
+++ src/main.zig
@@ -9,5 +9,11 @@ pub fn main() !void {
     }
     defer _ = c.sqlite3_close(optionalDb);
     const db: *c.sqlite3 = optionalDb orelse unreachable;
-    print(">> {d}", .{db});
+    print(">> {d}\n", .{db});
+
+    var stmt: ?*c.sqlite3_stmt = undefined;
+    _ = c.sqlite3_prepare_v2(db, "select 'hello';", -1, &stmt, null);
+    while (c.sqlite3_step(stmt) != c.SQLITE_DONE) {
+        print(">> {s}\n", .{c.sqlite3_column_text(stmt, 0)});
+    }
 }
